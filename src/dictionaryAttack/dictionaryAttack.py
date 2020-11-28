6be9505dfb034b802eeb4209d9e8bf2e5c84bd4a
Juan Jovel <juanjosejovel1999@outlook.com>
2020-11-28 02:40:41 -0500

    Added password file and a reader
    Added the .txt file with the passwords and created a reader.

--- src/dictionaryAttack/dictionaryAttack.py
+++ src/dictionaryAttack/dictionaryAttack.py
@@ -4,5 +4,27 @@ November 28, 2020
 @author: Juan Jovel
 @author: Sami Marzougui
 '''
+from pathlib import Path;
 
-# TODO: Start coding
+# Reads the passwords from the resource folder and returns a list containing all the passwords
+# without the newline character.
+def readFile() -> list:
+    
+    # Creates empty list of passwords.
+    passwords = [];
+    
+    # Finds the path outside of the current directory.
+    myAbsolutePath = str(Path(__file__).parent.parent.parent)
+    
+    # Specifies the path of the password .txt file
+    passwordFilePath = myAbsolutePath+"\\res\\10-million-password-list-top-1000.txt";
+    
+    # Opens the file
+    file = open(passwordFilePath, "r");
+    
+    # Adds every line to the list
+    for password in file:
+        passwords.append(password.strip("\n"));
+    
+    # Returns a list of all the passwords
+    return passwords;
