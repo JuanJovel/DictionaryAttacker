05d767a8f4dfc95a62bfd8bbc18084b78fab3d47
Juan Jovel <juanjosejovel1999@outlook.com>
2020-12-07 21:05:38 -0500

    Refactored GUI
     * Ignored warnings
    * Removed uneccesary method (replaced with lambda expression)
    * Renamed crackPassword argument and fixed type mismatch.
    * General documentation and refactoring.

--- src/guiInterface/GUI.py
+++ src/guiInterface/GUI.py
@@ -9,8 +9,8 @@ November 28, 2020
 
 '''
 
-from tkinter import *
-from tkinter import ttk
+from tkinter import *  # @UnusedWildImport
+from tkinter import ttk  # @Reimport
 from dictionaryAttack.DictionaryAttacker import DictionaryAttacker;
 import time;
 
@@ -23,32 +23,45 @@ def clicked_button1():
     crackPassword(password, myHashType)
 
 
-def crackPassword(str1: str, str2: str):
-    attacker = DictionaryAttacker(str1, str2);
+def crackPassword(password: str, hashType: int):
+    
+    #Instantiates a new DictionaryAttacker object.
+    attacker = DictionaryAttacker(password, hashType);
+    
     # Reads password list from file
     passwordList = attacker.readDictionary();
     tempList = passwordList
+    
+    # Starts a timer
     startTime = time.time();
 
     # Adjusts the list to rule out impossible words based on the password size.
     passwordList = attacker.optimizeDictionary(passwordList);
-
-    passwordFound = "";
+    
+    passwordFound = "";  # @UnusedVariable
+    
     # Preliminary attack: Will only work for a single word in the dictionary, no combinations.
     passwordFound = attacker.attack(passwordList);
-    # If the length of the password found after the preliminary attack is zero, then the password MUST be a combination.
+    
+    # If the length of the password found after the preliminary attack is zero, 
+    # then the password MUST be a combination.
     if (len(passwordFound) == 0):
         attacker.setIsCombination(True);
         passwordList = attacker.optimizeDictionary(attacker.readDictionary());
 
     # Brute-force combination attack.
     passwordFound = attacker.attack(passwordList);
-
+    
+    # Stop the timer.
     endTime = time.time();
+    
+    # Add the timer to the window.
     timeElapsed = "{:.2f}".format(endTime - startTime)
     if (timeElapsed == '0.00'):
         timeElapsed = '< 0.1'
     timeLabel.configure(text="Time Elapsed: " + str(timeElapsed) + " seconds")
+    
+    # Add info to the window.
     pwordFoundTextLabel.configure(text="Password found was: " + passwordFound)
     numGuessesTextLabel.configure(text="Number of Guesses: "+str(attacker.numberOfGuesses))
     dictSizeLabel.configure(text="Dictionary Size: " + str(len(tempList)))
@@ -61,37 +74,44 @@ def selectHash(hashTypeSelected: int):
         hashType = 512;
 
 
+# Create window element.
 root = Tk()
 root.title('Dictionary Attack')
-root.geometry('800x600')
+root.geometry('550x450')
 rootFont = 'Candara'
 style = ttk.Style()
 style.configure('.', font=('Candara', 14))
 
+# Create elements
+
+# Labels.
 subtitleLabel = ttk.Label(root, text="Try to Crack a Password", font = ('Candara', 16, "bold"))
 pwordTextLabel = ttk.Label(root, text="Enter password below:")
 hashTextLabel = ttk.Label(root, text="Select SHA Hashing Type:")
+pwordFoundTextLabel = ttk.Label(root, text='')
+numGuessesTextLabel = ttk.Label(root, text='')
+timeLabel = ttk.Label(root, text="")
+dictSizeLabel = ttk.Label(root, text = "")
 
+# Password Field
 passwordField = ttk.Entry(root, width=30)
+
+# Check boxes
 sha256CheckBox = ttk.Checkbutton(root, text='SHA256', command=lambda: selectHash(256))
 sha512CheckBox = ttk.Checkbutton(root, text='SHA512', command=lambda: selectHash(512))
 
-crackButton1 = ttk.Button(root, text='Crack', command=clicked_button1)
+# Crack Button
+crackButton = ttk.Button(root, text='Crack', command=lambda: crackPassword(passwordField.get(), hashType))
 
-pwordFoundTextLabel = ttk.Label(root, text='')
-numGuessesTextLabel = ttk.Label(root, text='')
 
+# Place elements.
 subtitleLabel.grid(column=1)
 hashTextLabel.grid(column=1)
 sha256CheckBox.grid(column=1)
 sha512CheckBox.grid(column=1)
 pwordTextLabel.grid(column=1, rowspan=10)
 passwordField.grid(column=1)
-timeLabel = ttk.Label(root, text="")
-dictSizeLabel = ttk.Label(root, text = "")
-
-crackButton1.grid(column=1)
-
+crackButton.grid(column=1)
 timeLabel.grid(column=1)
 dictSizeLabel.grid(column=1)
 pwordFoundTextLabel.grid(column=1)
